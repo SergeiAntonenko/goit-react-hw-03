62ab9a65a6bbee990ca68d511e270cf039a6f2b5
SergeiAntonenko <gerbar50@gmail.com>
2024-03-31 13:59:01 +0300

    hw 3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,52 @@
-import Profile from "../Profile/Profile.jsx";
-import FriendList from "../FriendList/FriendList.jsx";
-import TransactionHistory from "../TransactionHistory/TransactionHistory.jsx";
+import { useState, useEffect } from "react";
+import ContactForm from "../ContactForm/ContactForm.jsx";
+import SearchBox from "../SearchBox/SearchBox.jsx";
+import ContactList from "../ContactList/ContactList.jsx";
+import initialContacts from "../../contacts.json";
 import css from "./App.module.css";
-import userData from "./userData.json";
-import friends from "./friends.json";
-import transactions from "./transactions.json";
 
 const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const localStorageData = window.localStorage.getItem("contacts");
+    return localStorageData ? JSON.parse(localStorageData) : initialContacts;
+  });
+  const [searchValue, setSearchValue] = useState("");
+  const filteredContacts = contacts.filter(
+    (contact) =>
+      contact.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+      contact.number.toString().includes(searchValue)
+  );
+
+  useEffect(() => {
+    window.localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleChangeSearch = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleDeleteContact = (id) => {
+    const nextContacts = [...contacts];
+    const index = nextContacts.findIndex((contact) => contact.id === id);
+    nextContacts.splice(index, 1);
+    setContacts(nextContacts);
+  };
+
+  const handleAddTour = (contactItem) => {
+    setContacts((prevState) => {
+      return [...prevState, contactItem];
+    });
+  };
+
   return (
-    <div className={css.container}>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
+    <div className={css["container"]}>
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={handleAddTour} />
+      <SearchBox
+        searchValue={searchValue}
+        handleChangeSearch={handleChangeSearch}
       />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      <ContactList contacts={filteredContacts} onDelete={handleDeleteContact} />
     </div>
   );
 };
